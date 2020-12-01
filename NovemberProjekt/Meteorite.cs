bfd4c172c7c2ba6656db0addb3f680837ab50691
Niklas <niklas.ostman02@gmail.com>
2020-12-01 13:33:58 +0100

    Jag har börjat skapa Meteorite klassen. Har skapat en static metod som kan spawna in nya meteorites

--- NovemberProjekt/Meteorite.cs
+++ NovemberProjekt/Meteorite.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System;
 
 namespace NovemberProjekt
@@ -6,9 +7,15 @@ namespace NovemberProjekt
     {
         private int height = 100;
         private int width = 100;
+        private float xPos;
+        private float yPos;
+        public static List<Meteorite> allMeteorites = new List<Meteorite>();
 
         public void update(){
-            
+            yPos += 10;
+        }
+        static public void spawner(){
+            allMeteorites.Add(new Meteorite());
         }
     }
 }
